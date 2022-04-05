f09b95386c7654d424d41e9ba085ec2ada3e8aef
Tae Young <k55416325@gamil.com>
2022-04-05 23:51:26 +0900

    base.html 생성

--- meta4music/settings.py
+++ meta4music/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_page.apps.MainPageConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'meta4music.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['meta4music/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
